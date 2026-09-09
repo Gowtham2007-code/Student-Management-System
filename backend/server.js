764ebc57ce2134e1fae2f000347386c8dc5c7253
Gowtham <gowthampannem2007@gmail.com>
2026-09-09 23:28:07 +0530

    Add Express server and gitignore

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 5000;
+
+app.get("/", (req, res) => {
+    res.send("Student Management System API is running");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
